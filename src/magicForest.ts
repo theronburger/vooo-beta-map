7f5a787757c861f626d249c78037c15dea75e65e
theronburger <dev@theronburger.com>
2023-02-15 20:40:32 +0100

    message location rules

--- src/magicForest.ts
+++ src/magicForest.ts
@@ -25,6 +25,19 @@ WA.onInit().then(() => {
             WA.chat.sendChatMessage('Ill trade ya for a ring I found', 'An angry scarecrow');
             
         }
+
+        WA.chat.onChatMessage((userMessage => {
+            if (hasPumpkin) {                
+                if (userMessage.toLocaleLowerCase().indexOf("sure") > -1) {                
+                    WA.chat.sendChatMessage("Sweet!", 'An angry scarecrow');
+                    WA.chat.sendChatMessage("You lost one pumpkin", 'Inventory');
+                    WA.chat.sendChatMessage("You acquired one gold ring", 'Inventory');
+                    hasPumpkin = false;
+                    // hasRing = true;
+    
+                }   
+            }         
+        }));
     })
 
     WA.room.area.onEnter('scarecrowPumpkin').subscribe(() => {
@@ -43,31 +56,22 @@ WA.onInit().then(() => {
                 setTimeout(() => {
                     WA.chat.sendChatMessage(message, 'A friendly scarecrow');
                 }, totalDelay)
-            }))            
+            }))         
+            WA.chat.onChatMessage((userMessage => {
+                // console.log('The user typed a message', userMessage);
+                if (metPumpkinScarecrow) {                
+                    if (userMessage.toLocaleLowerCase().indexOf("sure") > -1) {                
+                        WA.chat.sendChatMessage("Ah thats really nice of you. Here, have a pumpkin!", 'A friendly scarecrow');
+                        WA.chat.sendChatMessage("You acquired one pumpkin", 'Inventory');
+                        hasPumpkin = true;
+                    }   
+                }         
+            }));   
         }       
         
     })
 
-    WA.chat.onChatMessage((userMessage => {
-        // console.log('The user typed a message', userMessage);
-        if (metPumpkinScarecrow) {                
-            if (userMessage.toLocaleLowerCase().indexOf("sure") > -1) {                
-                WA.chat.sendChatMessage("Ah thats really nice of you. Here, have a pumpkin!", 'A friendly scarecrow');
-                WA.chat.sendChatMessage("You acquired one pumpkin", 'Inventory');
-                hasPumpkin = true;
-            }   
-        } 
-        if (hasPumpkin) {                
-            if (userMessage.toLocaleLowerCase().indexOf("sure") > -1) {                
-                WA.chat.sendChatMessage("Sweet!", 'An angry scarecrow');
-                WA.chat.sendChatMessage("You lost one pumpkin", 'Inventory');
-                WA.chat.sendChatMessage("You acquired one gold ring", 'Inventory');
-                hasPumpkin = false;
-                // hasRing = true;
 
-            }   
-        }         
-    }));
 
     // WA.room.area.onLeave('clock').subscribe(closePopup)
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
